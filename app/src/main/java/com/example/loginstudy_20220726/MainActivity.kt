709916766a7731149cf50b20b8f1dc03c01ac53f
윤예은 <yeayen123@gmail.com>
2022-07-26 11:41:38 +0900

    속성 값 조회(get) / 대입 (set) 진행

--- app/src/main/java/com/example/loginstudy_20220726/MainActivity.kt
+++ app/src/main/java/com/example/loginstudy_20220726/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.loginstudy_20220726
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        signUpBtn.setOnClickListener {
+//            Toast.makeText(this, "회원가입 버튼 클릭", Toast.LENGTH_SHORT).show()
+//            emailTxt.text = "아이디"
+
+            emailEdt.setText("아이디")
+        }
+
+        loginBtn.setOnClickListener {
+            Log.d("로그인버튼", "클릭")
+//            emailTxt.text = passwordTxt.text
+            val email = emailEdt.text
+            val password = passwordEdt.text
+
+            Toast.makeText(this, email, Toast.LENGTH_SHORT).show()
+        }
     }
 }
